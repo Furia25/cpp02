c1276327b976aaf7d266959e1f56fe5e35006706
Valentin Durand <vdurand@z2r2p4.42lyon.fr>
2025-11-17 18:54:55 +0100

    ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -5,6 +5,18 @@ Fixed::Fixed() : _value(0)
 	std::cout << "Default constructor called" << std::endl;
 };
 
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_value = value << this->_mask;
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_value = (int)(roundf(value * (1 << this->_mask)));
+}
+
 Fixed::Fixed(const Fixed &other)
 {
 	std::cout << "Copy constructor called" << std::endl;
@@ -14,7 +26,7 @@ Fixed::Fixed(const Fixed &other)
 Fixed	&Fixed::operator=(const Fixed &other)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->_value = other.getRawBits();	
+	this->_value = other._value;	
 	return (*this);
 }
 
@@ -33,4 +45,20 @@ void	Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->_value = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return (this->_value / (float)(1 << this->_mask));
+}
+
+int Fixed::toInt(void) const
+{
+	return (this->_value >> this->_mask);
+}
+
+std::ostream&	operator<<(std::ostream& os, const Fixed &obj)
+{
+	os << obj.toFloat();
+	return (os);
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: vdurand <vdurand@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 14:31:47 by vdurand           #+#    #+#             */
-/*   Updated: 2025/11/15 15:27:32 by vdurand          ###   ########.fr       */
+/*   Updated: 2025/11/17 18:48:55 by vdurand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,29 @@
 # define DEFAULT_MASK	8
 
 # include <iostream>
+# include <cmath>
 
 class Fixed
 {
 public:
 	Fixed();
 	Fixed(const Fixed &other);
-	Fixed	&operator=(const Fixed &other);
+	Fixed(const int value);
+	Fixed(const float value);
 	~Fixed();
 	/* methods */
-	int		getRawBits(void) const;
-	void	setRawBits(int const raw);
+	int				getRawBits(void) const;
+	void			setRawBits(int const raw);
+	float			toFloat(void) const;
+	int				toInt(void) const;
+	/*Operators overload*/
+	Fixed			&operator=(const Fixed &other);
 private:
 	/* data */
 	static const int	_mask = DEFAULT_MASK;
 	int					_value;
 };
+
+std::ostream&	operator<<(std::ostream& os, const Fixed& obj);
 
 #endif // FIXED_HPP
