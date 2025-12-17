899003db6593f7c5c61b8f67ddbad67b4027d046
Furia25 <valentinledur@gmail.com>
2025-12-17 16:19:43 +0100

    dffds

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: vdurand <vdurand@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 16:26:36 by vdurand           #+#    #+#             */
-/*   Updated: 2025/12/16 17:11:26 by vdurand          ###   ########.fr       */
+/*   Updated: 2025/12/17 16:16:01 by vdurand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ bool	bsp(const Point a, const Point b, const Point c, const Point point)
 	float d1 = signed_area(point, a, b);
 	float d2 = signed_area(point, b, c);
 	float d3 = signed_area(point, c, a);
+
+	//Check for bad triangles
+	if (d1 + d2 + d3 == 0.0f)
+		return (false);
 
 	bool has_neg = (d1 < 0.0f) || (d2 < 0.0f) || (d3 < 0.0f);
 	bool has_pos = (d1 > 0.0f) || (d2 > 0.0f) || (d3 > 0.0f);
